b4920ecb894d47ceaa2304e9d864331baf99cf64
Marco Peluso <id@marcopeluso.com>
2009-10-10 11:29:04 +0200

    * Take audio session set up code from [DeepSleepPreventer -init] and put it in [DeepSleepPreventer -setUpAudioSession]. This gets called in [DeepSleepPreventer -init] now. * DeepSleepPreventer's audio session now is set up to allow mixing other audio with it. This greatly simplifies DeepSleepPreventer's use in applications, as you don't have to call [DeepSleepPreventer -stopPreventSleep] before playing audio and [DeepSleepPreventer -startPreventSleep] after playing audio anymore.

--- DeepSleepPreventer.h
+++ DeepSleepPreventer.h
@@ -6,17 +6,59 @@
 //  Copyright 2009 Marco Peluso. All rights reserved.
 //
 
+//===============================================
+#pragma mark -
+#pragma mark Import
+//===============================================
+
 #import <Foundation/Foundation.h>
+
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Forward Declaration
+//===============================================
 
 @class AVAudioPlayer;
 
-@interface DeepSleepPreventer : NSObject {
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Interface
+//===============================================
+
+@interface DeepSleepPreventer : NSObject
+{
 	AVAudioPlayer *audioPlayer;
 	NSTimer *preventSleepTimer;
 }
+
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Property
+//===============================================
+
 @property (nonatomic, retain) AVAudioPlayer *audioPlayer;
 @property (nonatomic, retain) NSTimer *preventSleepTimer;
-- (void)playPreventSleepSound;
+
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Public Methods
+//===============================================
+
+- (void)playPreventSleepSound;	// FIXXXME: make this private
 - (void)startPreventSleep;
 - (void)stopPreventSleep;
+- (void)setUpAudioSession;		// FIXXXME: make this private
+
 @end
--- DeepSleepPreventer.m
+++ DeepSleepPreventer.m
@@ -6,35 +6,59 @@
 //  Copyright 2009 Marco Peluso. All rights reserved.
 //
 
+//===============================================
+#pragma mark -
+#pragma mark Import
+//===============================================
+
 #import "DeepSleepPreventer.h"
 #import <AVFoundation/AVFoundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Implementation
+//===============================================
+
 @implementation DeepSleepPreventer
+
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Synthesize
+//===============================================
 
 @synthesize audioPlayer;
 @synthesize preventSleepTimer;
 
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Inits and DidLoads
+//===============================================
+
 - (id)init
 {
-    if ((self = [super init])) {
-		// Activate audio session
-		AudioSessionSetActive(true);
-		// Set up audio session, to prevent iPhone from deep sleeping, while playing sounds
-		UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback;
-		AudioSessionSetProperty (
-			kAudioSessionProperty_AudioCategory,
-			sizeof (sessionCategory), 
-			&sessionCategory
-		);
+    if ((self = [super init]))
+	{
+		[self setUpAudioSession];
 		
-		// Set up sound file
+		// Set up path to sound file
 		NSString *soundFilePath = [[NSBundle mainBundle] pathForResource:@"noSound"
 																  ofType:@"wav"];
 		NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:soundFilePath];
 		
 		// Set up audio player with sound file
-		self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+		self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL
+																  error:nil];
+		[fileURL release];
 		[self.audioPlayer prepareToPlay];
 		
 		// You may want to set this to 0.0 even if your sound file is silent.
@@ -44,35 +68,116 @@
     return self;
 }
 
-- (void)playPreventSleepSound {
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Dealloc and UnLoads
+//===============================================
+
+- (void)dealloc
+{
+	[preventSleepTimer release];
+	[audioPlayer release];
+	[super dealloc];
+}
+
+
+
+
+//===============================================
+#pragma mark -
+#pragma mark Public Methods
+//===============================================
+
+// FIXXXME: make this private
+- (void)playPreventSleepSound
+{
 	[self.audioPlayer play];
 }
 
-- (void)startPreventSleep {
+
+- (void)startPreventSleep
+{
 	// We need to play a sound at least every 10 seconds to keep the iPhone awake.
 	// We create a new repeating timer, that begins firing now and then every ten seconds.
 	// Every time it fires, it calls -playPreventSleepSound
 	self.preventSleepTimer = [[NSTimer alloc] initWithFireDate:[NSDate dateWithTimeIntervalSinceNow:0]
-												  interval:10.0
-													target:self
-												  selector:@selector(playPreventSleepSound)
-												  userInfo:nil
-												   repeats:YES];
+													  interval:5.0
+														target:self
+													  selector:@selector(playPreventSleepSound)
+													  userInfo:nil
+													   repeats:YES];
 	// We add this timer to the current run loop
 	NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
 	[runLoop addTimer:self.preventSleepTimer forMode:NSDefaultRunLoopMode];
 }
 
-- (void)stopPreventSleep {
+
+- (void)stopPreventSleep
+{
 	[self.preventSleepTimer invalidate];
 	self.preventSleepTimer = nil;
 }
 
-// Don't forget Memory management:
-- (void)dealloc {
-	[preventSleepTimer release];
-	[audioPlayer release];
-	[super dealloc];
+// FIXXXME: make this private
+- (void)setUpAudioSession
+{
+	// Initialize audio session
+		AudioSessionInitialize (
+			NULL,							// Use NULL to use the default (main) run loop.
+			NULL,							// Use NULL to use the default run loop mode.
+			NULL,							// A reference to your interruption listener callback function. See “Responding to Audio Session Interruptions” for a description of how to write and use an interruption callback function.
+			NULL							// Data you intend to be passed to your interruption listener callback function when the audio session object invokes it.
+		);
+		
+		// Activate audio session
+		OSStatus activationResult = 0;
+		activationResult = AudioSessionSetActive(true);
+		
+		if (activationResult)
+			DLog(@"AudioSession is active");
+
+		// Set up audio session category to kAudioSessionCategory_MediaPlayback.
+		// While playing sounds using this session category at least every 10 seconds, the iPhone doesn't go to sleep.
+		UInt32 sessionCategory = kAudioSessionCategory_MediaPlayback;	// Defines a new variable of type UInt32 and initializes it with the identifier 
+																		// for the category you want to apply to the audio session.
+		AudioSessionSetProperty (
+			kAudioSessionProperty_AudioCategory,						// The identifier, or key, for the audio session property you want to set.
+			sizeof(sessionCategory),									// The size, in bytes, of the property value that you are applying.
+			&sessionCategory											// The category you want to apply to the audio session.
+		);
+		
+		// Set up audio session playback mixing behavior.
+		// kAudioSessionCategory_MediaPlayback usually prevents playback mixing, so we allow it here. This way, we don't get in the way of other sound playback in an application.
+		// This property has a value of false (0) by default. When the audio session category changes, such as during an interruption, the value of this property reverts to false. To regain mixing behavior you must then set this property again.
+
+		// Always check to see if setting this property succeeds or fails, and react appropriately; behavior may change in future releases of iPhone OS.
+		OSStatus propertySetError = 0;
+		UInt32 allowMixing = true;
+ 
+		propertySetError =	AudioSessionSetProperty (
+								kAudioSessionProperty_OverrideCategoryMixWithOthers,	// The identifier, or key, for the audio session property you want to set.
+								sizeof(allowMixing),									// The size, in bytes, of the property value that you are applying.
+								&allowMixing											// The value to apply to the property.
+							);
+		
+		if (propertySetError)
+			DLog(@"Error setting kAudioSessionProperty_OverrideCategoryMixWithOthers: %d", propertySetError);
+
+		// FIXXXME: This should be done, when 
+//		// Set up audio session output route overriding. 
+//		propertySetError = 0;
+//		UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
+// 
+//		propertySetError =	AudioSessionSetProperty (
+//								kAudioSessionProperty_O,
+//								sizeof(doChangeDefaultRoute),
+//								&doChangeDefaultRoute
+//							);
+//		if (propertySetError)
+//			DLog(@"Error setting kAudioSessionProperty_OverrideCategoryMixWithOthers: %d", propertySetError);
 }
 
 @end
